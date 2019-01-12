8a7b5c53848f023dd39ec53bfccc17bbcf8606d8
forrequia <orrequia8@gmail.com>
2019-01-12 12:30:57 +0100

    Eliminado importaciones no utilizadas.

--- src/app/service/company/company.service.ts
+++ src/app/service/company/company.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/index';
-import { HttpClient } from '@angular/common/http';
 import {GenericService} from '../generic.service';
 
 @Injectable()
