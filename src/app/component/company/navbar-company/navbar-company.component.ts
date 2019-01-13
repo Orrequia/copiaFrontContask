58bb5312b5ef63022ad7c40d4ddc7a1bf258a9f1
forrequia <orrequia8@gmail.com>
2019-01-13 10:41:58 +0100

    Añadido logout

--- src/app/component/company/navbar-company/navbar-company.component.ts
+++ src/app/component/company/navbar-company/navbar-company.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../../service/auth/auth.service';
 
 @Component({
   selector: 'app-navbar-company',
@@ -8,8 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarCompanyComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  logout() {
+    this.authService.logout();
   }
 }
