cb11f0b6f77a22f2bd75153ceb5d5660f1c820c3
forrequia <orrequia8@gmail.com>
2019-01-15 16:23:41 +0100

    Detalles de Company

--- src/app/component/company/detail-company/detail-company.component.ts
+++ src/app/component/company/detail-company/detail-company.component.ts
@@ -1,28 +1,38 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {Company} from '../../../model/company.model';
 import {CompanyTypeService} from '../../../service/company-type/company-type.service';
-import {CompanyType} from '../../../model/company-type';
-import {Observable} from 'rxjs';
+import {CompanyType} from '../../../model/company-type.model';
+import {EmployeeService} from '../../../service/employee/employee.service';
+import {Employee} from '../../../model/employee.model';
 
 @Component({
   selector: 'app-detail-company',
   templateUrl: './detail-company.component.html',
   styleUrls: ['./detail-company.component.css']
 })
-export class DetailCompanyComponent implements OnInit {
+export class DetailCompanyComponent implements OnInit, OnChanges {
 
   @Input() company: Company;
   companyType: CompanyType;
+  owner: Employee;
 
-  constructor(private companyTypeService: CompanyTypeService) { }
+  constructor(private companyTypeService: CompanyTypeService,
+              private employeeService: EmployeeService) {
+    this.companyType = new CompanyType();
+    this.owner = new Employee();
+  }
 
   ngOnInit() {
-    console.log(this.company);
-    // this.companyType = this.companyTypeService.get(this.company.idCompanyType) as unknown as Observable<CompanyType>;
+  }
 
+  ngOnChanges(changes: SimpleChanges): void {
     this.companyTypeService.get(this.company.idCompanyType).subscribe(companyType => {
       this.companyType = companyType as unknown as CompanyType;
     });
+    this.employeeService.get(this.company.idOwner).subscribe(employee => {
+      this.owner = employee as unknown as Employee;
+    });
   }
+
 
 }
---
+++ src/app/model/employee.model.ts
@@ -0,0 +1,6 @@
+export class Employee {
+  idEmployee: number;
+  name: string;
+  phone: string;
+  email: string;
+}
