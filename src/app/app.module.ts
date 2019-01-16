2a78e6899c95033aa2d6f91b706ca972fe748bec
forrequia <orrequia8@gmail.com>
2019-01-16 17:11:45 +0100

    Añadido el nuevo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {HttpClientModule} from '@angular/common/http';
 import { NavbarCompanyComponent } from './component/company/navbar-company/navbar-company.component';
 import { ListCompanyComponent } from './component/company/list-company/list-company.component';
 import { DetailCompanyComponent } from './component/company/detail-company/detail-company.component';
+import { DetailStoreComponent } from './component/store/detail-store/detail-store.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { DetailCompanyComponent } from './component/company/detail-company/detai
     LoginComponent,
     NavbarCompanyComponent,
     ListCompanyComponent,
-    DetailCompanyComponent
+    DetailCompanyComponent,
+    DetailStoreComponent
   ],
   imports: [
     BrowserModule,
